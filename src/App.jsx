7a7334f9f9563c1c9db8c3a1d48279fec2f90c50
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2026-03-12 23:37:03 -0300

    Refatorar código para melhorar a legibilidade e consistência na formatação.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function App() {
-
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
   const [usuarios, setUsuarios] = useState([]);
 
   function handleSubmit(event) {
@@ -13,33 +12,57 @@ export default function App() {
     setUsuarios([...usuarios, { nome, email, senha }]);
   }
 
-  return(
+  return (
     <div className="App">
       <h1>Cadastro de Usuários</h1>
 
       <form action="">
         <div>
           <label htmlFor="nome">Nome:</label>
-          <input type="text" id="nome" name="nome" value={nome} onChange={(e) => setNome(e.target.value)}/>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="senha">Senha:</label>
-          <input type="password" id="senha" name="senha" value={senha} onChange={(e) => setSenha(e.target.value)}/>
+          <input
+            type="password"
+            id="senha"
+            name="senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
         </div>
         <button type="submit" onClick={handleSubmit}>
           Cadastrar
         </button>
       </form>
 
-      <div className="usuarios-cadastrados">
-        {usuarios.map((usuario, index) => (
-          <p key={index}><span>👤 {usuario.nome}</span> <span>✉️ {usuario.email}</span> <span>🗑</span></p>
-        ))}
-      </div>
+      {usuarios.length > 0 && (
+        <div className="usuarios-cadastrados">
+          {usuarios.map((usuario, index) => (
+            <p key={index}>
+              <span>👤 {usuario.nome}</span>
+              <span>✉️ {usuario.email}</span>
+              <span>🗑</span>
+            </p>
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
